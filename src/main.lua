d44e5c2f7da04b1a1d7591784ed2b0a2e0d4d84f
JameDevOfficial <jame.bayerl@gmail.com>
2025-10-29 18:12:01 +0100

    init repo

---
+++ src/main.lua
@@ -0,0 +1,19 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.resize()
+
+end
+
+function love.keypressed(key, scancode, isrepeat)
+
+end
